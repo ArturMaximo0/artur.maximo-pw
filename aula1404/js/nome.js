201831c5025f21a84db62e47968465e30fb18822
Artur Maximo <artur_fonseca@outlook.com>
2026-04-28 18:43:07 -0300

    Revert "Aula do dia 28/04"
    This reverts commit d809a52c67d1f4c6a8a39646bf66c687b819d785.

---
+++ aula1404/js/nome.js
@@ -0,0 +1,7 @@
+var nome = "Artur";
+var nome2 = prompt("digite seu nome: ");
+var idade = parseInt(prompt("Digite sua idade: "));
+
+console.log("seu nome é: ", nome);
+alert("Seu nome é: "+ nome2);
+alert("Sua idade é: "+ idade);
---
+++ aula1404/js/soma.js
@@ -0,0 +1,5 @@
+var numero = parseFloat(prompt("Digite o primeiro número: "));
+var numero2 = parseFloat(prompt("Digite o segundo número: "));
+var soma = numero + numero2;
+
+console.log("A soma dos números é: "+ soma);
