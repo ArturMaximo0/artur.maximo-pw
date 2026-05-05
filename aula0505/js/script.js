13879efbc0d2c16fddf220183a1ecb7083e936c4
Artur Maximo <artur_fonseca@outlook.com>
2026-05-05 08:37:37 -0300

    aula 05/05

---
+++ aula0505/js/script.js
@@ -0,0 +1,31 @@
+// estrutura de repetição while
+var x = 0;
+var num = 0;
+while(x < 5){
+    x++
+    num = x + num;
+    
+    
+}
+
+console.log(num);
+
+// cria um array passando por todos os elementos
+var arr = ['teste1', 'teste2', 'teste3', 'a'];
+
+var y = 0;
+
+//passando por todos os elementos
+while (y <= 3){
+    console.log(arr[y]);
+    y++
+}
+
+//passando por todas as palavras
+var palavra = 'ARTUR'
+var i = 0;
+//passando por todos os elementos
+while (i < 5){
+    console.log(palavra[i]);
+    i++
+}
--- aula2804/js/calculo.js
+++ aula2804/js/calculo.js
@@ -11,8 +11,38 @@ function subtrair() {
 }
 function multiplicar() {
     resultado.innerHTML = Number(n1.value) * Number(n2.value);
+    
 }
 function dividir() {
-    resultado.innerHTML = Number(n1.value) / Number(n2.value);
+    Number(n1.value) / Number(n2.value);
+    if (n1.value == 0 || n2.value == 0){
+        resultado.innerHTML = 'Erro divisão por 0'
+    }
+    else {
+        resultado.innerHTML = Number(n1.value) / Number(n2.value);
+    }
 }
-
+function areaT() {
+    resultado.innerHTML = (Number(n1.value) * Number(n2.value)) / 2;
+}
+function areaQ() {
+    if (n1.value != n2.value){
+        resultado.innerHTML = 'Os lados tem que ser iguais'
+    }else{
+    resultado.innerHTML = Number(n1.value) * Number(n1.value);
+    }
+}
+function idade() {
+    if(n1.value < 18){
+        resultado.innerHTML = 'Menor de idade';
+    }else {
+        resultado.innerHTML = 'Maior de idade';
+    }
+}
+function par() {
+    if ((n1.value % 2) == 0){
+        resultado.innerHTML = 'Par'
+    }else{
+        resultado.innerHTML = 'Impar'
+    }
+}
